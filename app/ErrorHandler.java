54407d251a03df761be77351c4e9e1120ab03c18
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2016-05-13 21:17:54 -0300

    Redirecionamento para tela correta caso a rota acessada não exista.

--- app/ErrorHandler.java
+++ app/ErrorHandler.java
@@ -1,24 +1,25 @@
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
+import controllers.routes;
 import play.http.*;
 import play.mvc.Http.RequestHeader;
 import play.mvc.Result;
 import play.mvc.Results;
 
-public class PageNotFoundError implements HttpErrorHandler {
+public class ErrorHandler implements HttpErrorHandler {
 
 	@Override
 	public CompletionStage<Result> onClientError(RequestHeader request, int statusCode, String message) {
 		return CompletableFuture.completedFuture(
-                Results.status(statusCode, "A client error occurred: " + message)
+                Results.redirect(routes.HomeController.index())
         );
 	}
 
 	@Override
 	public CompletionStage<Result> onServerError(RequestHeader request, Throwable exception) {
 		return CompletableFuture.completedFuture(
-                Results.internalServerError("A server error occurred: " + exception.getMessage())
+				Results.redirect(routes.HomeController.index())
         );
 	}
 
